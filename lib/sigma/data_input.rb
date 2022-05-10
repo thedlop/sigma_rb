11ce7b60e5b3db2608c0bbb19a20fdb10511b723
Dark Lord of Programming <thedlop@sent.com>
2022-05-10 16:30:51 +0900

    Finish DataInputs

--- lib/sigma/data_input.rb
+++ lib/sigma/data_input.rb
@@ -46,7 +46,54 @@ module Sigma
     extend FFI::Library
     ffi_lib File.join(File.dirname(__FILE__), "../../ext/libsigma.so")
     typedef :pointer, :error_pointer
+    attach_function :ergo_lib_data_inputs_new, [:pointer], :void
+    attach_function :ergo_lib_data_inputs_delete, [:pointer], :void
+    attach_function :ergo_lib_data_inputs_add, [:pointer, :pointer], :void
+    attach_function :ergo_lib_data_inputs_len, [:pointer], :uint8
+    attach_function :ergo_lib_data_inputs_get, [:pointer, :uint8, :pointer], ReturnOption.by_value
     attr_accessor :pointer
 
+    def self.with_raw_pointer(unread_pointer)
+      init(unread_pointer)
+    end
+
+    def self.create
+      pointer = FFI::MemoryPointer.new(:pointer)
+      ergo_lib_data_inputs_new(pointer)
+
+      init(pointer)
+    end
+
+    def len
+      ergo_lib_data_inputs_len(self.pointer)
+    end
+
+    def add(data_input)
+      ergo_lib_data_inputs_add(data_input.pointer, self.pointer)
+    end
+
+    def get(index)
+      pointer = FFI::MemoryPointer.new(:pointer)
+      res = ergo_lib_data_inputs_get(self.pointer, index, pointer)
+      Util.check_error!(res[:error])
+      if res[:is_some]
+        Sigma::DataInput.with_raw_pointer(pointer)
+      else
+        nil
+      end
+    end
+
+    private
+
+    def self.init(unread_pointer)
+      obj = self.new
+      obj_ptr = unread_pointer.get_pointer(0)
+
+      obj.pointer = FFI::AutoPointer.new(
+        obj_ptr,
+        method(:ergo_lib_data_inputs_delete)
+      )
+      obj
+    end
   end
 end
--- tests/sigma/data_input_test.rb
+++ tests/sigma/data_input_test.rb
@@ -16,5 +16,23 @@ class Sigma::DataInput::Test < Test::Unit::TestCase
   end
 
   def test_data_inputs
+    # create
+    data_inputs = assert_nothing_raised do
+      Sigma::DataInputs.create
+    end
+    # len
+    assert_equal(0, data_inputs.len)
+    # get
+    assert_equal(nil, data_inputs.get(0))
+    assert_equal(nil, data_inputs.get(1))
+    # add
+    str = "e56847ed19b3dc6b72828fcfb992fdf7310828cf291221269b7ffc72fd66706e"
+    box_id = Sigma::BoxId.with_string(str)
+    data_input = Sigma::DataInput.with_box_id(box_id) 
+    data_inputs.add(data_input)
+    assert_equal(1, data_inputs.len)
+    # no eq method for DataInput
+    assert_equal(box_id, data_inputs.get(0).get_box_id)
+    assert_equal(nil, data_inputs.get(1))
   end
 end
