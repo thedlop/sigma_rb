a593d0c9bf0d68733768fe4968537c5e492d6996
Dark Lord of Programming <thedlop@sent.com>
2022-05-24 07:00:46 +0900

    Added documentation to constant.rb

--- lib/sigma/constant.rb
+++ lib/sigma/constant.rb
@@ -3,6 +3,7 @@ require_relative './util.rb'
 require 'ffi-compiler/loader'
 
 module Sigma
+  # Ergo constant (evaluated) values
   class Constant
     extend FFI::Library
     ffi_lib FFI::Compiler::Loader.find('csigma')
@@ -20,6 +21,9 @@ module Sigma
     attach_function :ergo_lib_constant_delete, [:pointer], :void
     attr_accessor :pointer
 
+    # Create from ergo_box value
+    # @param ergo_box [ErgoBox]
+    # @return [Constant]
     def self.with_ergo_box(ergo_box)
       c_ptr = FFI::MemoryPointer.new(:pointer)
       ergo_lib_constant_from_ergo_box(ergo_box.pointer, c_ptr)
@@ -27,6 +31,9 @@ module Sigma
       init(c_ptr)
     end
 
+    # Create from byte array
+    # @param bytes [Array<uint8>] Array of unsigned 8-bit (0-255) integers
+    # @return [Constant]
     def self.with_bytes(bytes)
       c_ptr = FFI::MemoryPointer.new(:pointer)
       b_ptr = FFI::MemoryPointer.new(:uint8, bytes.size)
@@ -37,6 +44,9 @@ module Sigma
       init(c_ptr)
     end
 
+    # Parse raw EcPoint value from bytes and make ProveDlog constant
+    # @param bytes [Array<uint8>] Array of unsigned 8-bit (0-255) integers
+    # @return [Constant]
     def self.with_ecpoint_bytes(bytes)
       c_ptr = FFI::MemoryPointer.new(:pointer)
       b_ptr = FFI::MemoryPointer.new(:uint8, bytes.size)
@@ -47,20 +57,9 @@ module Sigma
       init(c_ptr)
     end
 
-    def self.with_raw_pointer(constant_pointer)
-      init(constant_pointer)
-    end
-
-    def to_base16_string
-      s_ptr = FFI::MemoryPointer.new(:pointer, 1)
-      error = ergo_lib_constant_to_base16(self.pointer, s_ptr)
-      Util.check_error!(error)
-      s_ptr = s_ptr.read_pointer()
-      str = s_ptr.read_string().force_encoding('UTF-8')
-      Util.ergo_lib_delete_string(s_ptr)
-      str
-    end
-
+    # Create from 32-bit integer
+    # @param int [Integer]
+    # @return [Constant]
     def self.with_i32(int)
       pointer = FFI::MemoryPointer.new(:pointer)
       error = ergo_lib_constant_from_i32(int, pointer)
@@ -69,6 +68,9 @@ module Sigma
       init(pointer)
     end
 
+    # Create from 64-bit integer
+    # @param int [Integer]
+    # @return [Constant]
     def self.with_i64(int)
       pointer = FFI::MemoryPointer.new(:pointer)
       error = ergo_lib_constant_from_i64(int, pointer)
@@ -85,18 +87,45 @@ module Sigma
       init(c_ptr)
     end
 
+    # Takes ownership of an existing Constant Pointer.
+    # @note A user of sigma_rb generally does not need to call this function
+    # @param pointer [FFI::MemoryPointer]
+    # @return [Constant]
+    def self.with_raw_pointer(constant_pointer)
+      init(constant_pointer)
+    end
+
+    # Encode as Base16-encoded ErgoTree serialized value or throw an error if serialization failed
+    # @return [String] Base-16 encoded ErgoTree serialized value
+    def to_base16_string
+      s_ptr = FFI::MemoryPointer.new(:pointer, 1)
+      error = ergo_lib_constant_to_base16(self.pointer, s_ptr)
+      Util.check_error!(error)
+      s_ptr = s_ptr.read_pointer()
+      str = s_ptr.read_string().force_encoding('UTF-8')
+      Util.ergo_lib_delete_string(s_ptr)
+      str
+    end
+
+    # Extract 32-bit integer value, throw error if wrong type
+    # @return [Integer]
     def to_i32
       res = ergo_lib_constant_to_i32(self.pointer)
       Util.check_error!(res[:error])
       res[:value]
     end
 
+    # Extract 64-bit integer value, throw error if wrong type
+    # @return [Integer]
     def to_i64
       res = ergo_lib_constant_to_i64(self.pointer)
       Util.check_error!(res[:error])
       res[:value]
     end
 
+    # Equality check for two Constants
+    # @param constant_two [Constant]
+    # @return [Constant]
     def ==(constant_two)
       ergo_lib_constant_eq(self.pointer, constant_two.pointer)
     end
